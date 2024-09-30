e0b12f9792c73c6293df82edfa812ec19dbd629e
Vinayak Pawar <vinayak.pawar98765@gmail.com>
2024-09-30 16:03:28 +0530

    Add files via upload

---
+++ DAY - 3 Break the stick - Medium.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int T, N, X;
+    scanf("%d", &T);
+    while (T--) {
+        scanf("%d %d", &N,&X);
+        int isPossible = 0;
+        if (X % 2 != 0 && N % 2 == 0) {
+            isPossible = 0;
+        } else {
+        if (X % 2 == 0 && N % 2 != 0) {
+            isPossible = 0;
+            } else {
+                if(X > N) {
+                    isPossible = 0;
+                } else {
+                    if (X == N) {
+                        ifPossible = 1;
+                    } else {
+                        while (N > X) {
+                            if (N % 2 !=0)
+                            {
+                                isPossible = 0;
+                                break;
+                            }
+                            N /= 2;
+                        }
+                        if (N != X) {
+                            isPossible = 0;
+                        }
+                    }
+                }
+
+            }
+        }
+            if (isPossible) {
+                printf("YES\n");
+            } else {
+                printf("NO\n");
+            }
+        }
+        return 0;
+    }
